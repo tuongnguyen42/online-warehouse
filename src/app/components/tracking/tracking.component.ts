bb75ad4abc47b0115a393f9ada43440491be8a8f
serenapascual <serena.a.pascual@gmail.com>
2019-04-27 15:33:46 -0700

    Shows markers this time

--- src/app/components/tracking/tracking.component.ts
+++ src/app/components/tracking/tracking.component.ts
@@ -1,4 +1,6 @@
+/// <reference path="./../../../../node_modules/@types/googlemaps/index.d.ts"/>
 import { Component, OnInit } from '@angular/core';
+import { AgmCoreModule, MapsAPILoader } from "@agm/core";
 
 @Component({
   selector: 'app-tracking',
@@ -7,13 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TrackingComponent implements OnInit {
-  // Warehouse 1
-  latitude = 37.3047208;
-  longitude = -121.862952;
-  mapType = 'roadmap';
+  mexicoCity = null
+  jacksonville = null
 
-  constructor() { }
+  // Warehouse 1 { lat: 37.3047208, lng: -121.862952 }
+  // Warehouse 2 { lat: 37.3346377, lng: -121.9819636 }
+
+  lat: Number = 37.3047208;
+  lng: Number = -121.862952;
+  origin = { lat: 37.3047208, lng: -121.862952 }
+  destination = { lat: 37.338888, lng: -121.881553 }
+  waypoints: object = [
+    {
+        location: { lat: 37.3346377, lng: -121.9819636 },
+        stopover: true,
+    },
+  ]
+
+  constructor(private mapsAPILoader: MapsAPILoader) {
+    this.mapsAPILoader.load().then(() => {
+      this.mexicoCity = new google.maps.LatLng(19.432608, -99.133209);
+      this.jacksonville = new google.maps.LatLng(40.730610, -73.935242);
+    });
+  }
 
   ngOnInit() {
+    console.log(this.calculateDistance());
+  }
+
+  calculateDistance() {
+    const distance = google.maps.geometry.spherical.computeDistanceBetween(this.mexicoCity, this.jacksonville);
+    return distance;
   }
 }
