b23d4b98e5e32519677c5357e0d7aaa5ee743faa
Edrees Osman <edosman@Edreess-MacBook-Pro.local>
2019-04-29 15:24:56 -0700

    connect.py

--- backend/venv/connect.py
+++ backend/venv/connect.py
@@ -3,10 +3,9 @@ import mysql.connector
 def cursor_connect():
     cnx = mysql.connector.connect(
     user='root',
-    password='Chungu1234',
+    password='password',
     host='localhost',
-    database='onlinewarehouse',
-    port='3000'
+    database='onlinewarehouse'
     )
     cur = cnx.cursor(buffered=True)
     return cur, cnx
