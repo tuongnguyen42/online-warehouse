6b51ac32b4aac02220f9c8d53f5fdb1d86b36760
Tuong Nguyen <tuong.nguyen42@yahoo.com>
2019-05-11 20:25:36 -0700

    tracking implemented

--- backend/venv/orders.py
+++ backend/venv/orders.py
@@ -1,5 +1,7 @@
 from connect import cursor_connect
 import json
+import googlemaps
+
 
 def get_orders_by_user(id):
     cursor, cnx = cursor_connect()
@@ -22,31 +24,27 @@ def get_orders_by_user(id):
     cnx.close()
     return json_orders
 
-
 def get_tracking_by_order(id):
     cursor, cnx = cursor_connect()
     cursor.execute(
-        """SELECT tracking_id, order_id, origin, stopover, address, city, state, zip, delivery_method, delivery_status FROM tracking WHERE order_id=%s""",
+        """SELECT lat, lng, delivery_method, delivery_status, purchase_time FROM orders WHERE order_id=%s""",
         (id,))
     tracking = cursor.fetchall()
-    json_tracking = []
-    for value in tracking:
-        dict = {"tracking_id": value[0],
-              "order_id": value[1],
-              "origin": value[2],
-              "stopover": value[3],
-              "address": value[4],
-              "city": value[5],
-              "state": value[6],
-              "zip": value[7],
-              "delivery_method": value[8],
-              "delivery_status": value[9]
+    print(tracking[0])
+    if tracking:
+        info = {
+              "lat":str(tracking[0][0]),
+              "lng":str(tracking[0][1]),
+              "delivery_status": tracking[0][2],
+              "delivery_method": tracking[0][3],
+              "purchase_time": tracking[0][4]
+
               }
-        json_tracking.append(dict)
+
 
     cursor.close()
     cnx.close()
-    return json_tracking
+    return info
 
 
 # get_orders_by_user(1)
@@ -65,17 +63,23 @@ def existing_order(order_number):
         return False
 
 ### order history
-def new_order(a_id, items, tot_price, tot_weight):
+def new_order(a_id, items, tot_price, tot_weight, address, delivery_method, delivery_status):
     cursor, cnx = cursor_connect()
+    gmaps_key = googlemaps.Client(key = "AIzaSyALPpI2grWR5poDZf4JMpHDLMcAHAwZ6R0")
+    geocode = gmaps_key.geocode(address)
+
     try:
-        cursor.execute("""INSERT INTO orders (account_id, items, total_price, total_weight) VALUES (%s,%s,%s,%s)""",
-        (a_id, items, tot_price, tot_weight))
+        lng = geocode[0]["geometry"]["location"]["lng"]
+        lat = geocode[0]["geometry"]["location"]["lat"]
+        cursor.execute("""INSERT INTO orders (account_id, items, total_price, total_weight, lat, lng, delivery_method,  delivery_status) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""",
+        (a_id, items, tot_price, tot_weight, lat, lng, delivery_method, delivery_status))
+
         cnx.commit()
         # print("new order added")
 
     except Exception as e:
         print("order unable to be added:")
-        # print(e.message)
+        print(e)
         cursor.close()
         cnx.close()
         return False
@@ -84,22 +88,25 @@ def new_order(a_id, items, tot_price, tot_weight):
     cnx.close()
     return True
 
-def get_order_by_account(a_id):
-    cursor, cnx = cursor_connect()
-    cursor.execute("""SELECT order_id, purchase_time, items, total_price, total_weight FROM orders WHERE account_id = %s ORDER BY purchase_time DESC""", (a_id,))
 
-    orders = cursor.fetchall()
-    json_orders = []
 
-    for order in orders:
-        o = {"order_id" : order[0],
-             "purchase_time" : order[1],
-             "items": order[2],
-             "total_price": order[3],
-             "total_weight": order[4]
-        }
-        json_orders.append(o)
 
-    cursor.close()
-    cnx.close()
-    return json_orders
+# def get_order_by_account(a_id):
+#     cursor, cnx = cursor_connect()
+#     cursor.execute("""SELECT order_id, purchase_time, items, total_price, total_weight FROM orders WHERE account_id = %s ORDER BY purchase_time DESC""", (a_id,))
+#
+#     orders = cursor.fetchall()
+#     json_orders = []
+#
+#     for order in orders:
+#         o = {"order_id" : order[0],
+#              "purchase_time" : order[1],
+#              "items": order[2],
+#              "total_price": order[3],
+#              "total_weight": order[4]
+#         }
+#         json_orders.append(o)
+#
+#     cursor.close()
+#     cnx.close()
+#     return json_orders
